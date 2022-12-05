f40ec263b7133f85a567ebfcd77dc78e377e059e
Rafael Maturana <84340401+Rafosox@users.noreply.github.com>
2022-12-04 22:33:44 -0300

    Create jwtInstalls.js

---
+++ jwtInstalls.js
@@ -0,0 +1,42 @@
+function generateAccessToken(username) {
+  return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
+}
+
+  app.post('/api/createNewUser', (req, res) => {
+  // ...
+
+  const token = generateAccessToken({ username: req.body.username });
+  res.json(token);
+
+  // ...
+});
+
+//--------
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.TOKEN_SECRET as string, (err: any, user: any) => {
+    console.log(err)
+
+    if (err) return res.sendStatus(403)
+
+    req.user = user
+
+    next()
+  })
+}
+app.get('/api/userOrders', authenticateToken, (req, res) => {
+  // executes after authenticateToken
+  // ...
+})
+
+//-------------
+
+// get token from fetch request
+const token = await res.json();
+
+// set token in cookie
+document.cookie = `token=${token}`
