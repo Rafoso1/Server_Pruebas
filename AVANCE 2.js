8bea59d0bfd205b49a0d2216301048803ebe9e3d
Rafael Maturana <84340401+Rafosox@users.noreply.github.com>
2022-12-04 22:07:50 -0300

    Update AVANCE 2.js

--- AVANCE 2.js
+++ AVANCE 2.js
@@ -54,6 +54,7 @@ app.post('/login', async (req, res) => {
 //va a recibir el parametro id
 app.get('/user/:id', (req, res) => {
     var id = req.params.id;
+    //res.json(id);
     //recibe un token y retorna los datos del usuario
     //si el token es correcto
 
@@ -97,7 +98,8 @@ app.get('/eventos', async (req, res) => {
 
 app.get('/eventos/:id', (req, res) => {
     var id = req.params.id;
-    //INSERTE FIND/FINDONE MONGODB  
+    //INSERTE FIND/FINDONE MONGODB 
+
     let token = req.query.token || req.headers['authorization'];
     if (token) {
         jwt.verify(token, 'secret',
@@ -156,7 +158,131 @@ app.post('/eventos/comprar/:id/:asiento/:iduser', (req, res) => {
         res.json({ ok: false, error: 'No token provided.' });
     } 
 });
+//---------------------------------------------------------------------------------------------------
+// app.get('/login/:correo&&:contra',(req,res) =>{
+//     var correo= req.params.email;
+//     var contra = req.params.contra;
+//     //INSERTE FIND/FINDONE MONGODB  
+//     var resultado=false
+//     if(resultado){
+//       req.cookies.email=correo;
+//       //req.cookies.name=usuario.name;
+      
+//       console.log(req.cookies);
+//       res.status(200).json({
+//         "Estado" : "Logeado con exito",
+//         "Correo": req.cookies.email
+//       })
+//     }else{
+//       res.status(200).json({
+//         "Estado" : "El usuario no existe"
+//       })
+//     }
+//   })
+// ---------------------------------------------------------------------------------------------------
+// app.get("/API/eventos", async (req, res) => {
+// 	try {
+// 		const collection = database.collection("eventos");
+// 		collection.find({}).toArray(function (err, result) {
+// 			if (err) {
+// 				console.log(err);
+// 			} else {
+// 				console.log("Query exitosa a la db, eventos enviados");
+// 				res.json({
+// 					eventos: result,
+// 				});
+// 				return;
+// 			}
+// 		});
 
+// 		//res.send(usuario);
+// 	} finally {
+// 		// Ensures that the client will close when you finish/error
+// 		//await client.close();
+// 		console.log("Error en la query");
+// 	}
+// });
+
+// function loginByToken(req) {
+// 	const token = req.headers.authorization;
+// 	if (!token) return null;
+// 	const decoded = jwt.verify(token, SECRET_JWT_KEY);
+// 	return decoded;
+// }
+
+// app.post("/API/login", (req, res) => {
+// 	if (loginByToken(req)) {
+// 		res.status(200).json({
+// 			message: "Login con token exitoso!",
+// 		});
+// 		return;
+// 	}
+// 	//Ejemplo de token para Frez
+// 	// https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiRnJleiIsIm5hbWUiOiJtaW5lY3JhZnQifQ.NfI48yK1xd3LAQJz07tDRSIjmdFX3v4_UZPbM9Ys9Io
+
+// 	//Recibe datos desde el cliente
+// 	const request = JSON.parse(JSON.stringify(req.body));
+
+// 	//verifico que request.user y request.password no esten vacios
+// 	if (request.user && request.password) {
+// 		const user = request.user;
+// 		const password = request.password;
+
+// 		//consulto a la db si existe el usuario
+// 		const collection = database.collection("usuarios");
+// 		collection.find({ usuario: user }).toArray(function (err, result) {
+// 			if (err) {
+// 				console.log(err);
+// 			} else {
+// 				//si existe el usuario, verifico que la password sea correcta
+// 				if (result[0].password == password) {
+// 					//creo el token
+// 					const token = jwt.sign(
+// 						{ user: user, id: result[0]._id },
+// 						SECRET_JWT_KEY,
+// 						{ expiresIn: "1h" }
+// 					);
+// 					res.status(200).json({
+// 						token: token,
+// 					});
+// 					return;
+// 				} else {
+// 					res.json({
+// 						message: "Contraseña incorrecta",
+// 					});
+// 					return;
+// 				}
+// 			}
+// 		});
+// 	} else {
+// 		res.status(400).json({
+// 			message: "Datos incompletos",
+// 		});
+// 		return;
+// 	}
+// });
+
+// //Recibe un id de usuarioy retorna los datos del usuario
+// app.post("/API/user/:id", (req, res) => {
+// 	if (!loginByToken(req)) {
+// 		res.status(401).json({
+// 			message: "Token invalido",
+// 		});
+// 		return;
+// 	}
+// 	const id = req.params.id;
+// 	const collection = database.collection("usuarios");
+// 	collection.find({ _id: ObjectID(id) }).toArray(function (err, result) {
+// 		if (err) {
+// 			console.log(err);
+// 		} else {
+// 			res.json({
+// 				usuario: result[0],
+// 			});
+// 			return;
+// 		}
+// 	});
+// });
 
 app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
